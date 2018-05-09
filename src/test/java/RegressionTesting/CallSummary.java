cff9037159b830bc43f406a2a2d5c3bd96dcfba8
anishakumar <anish.a.kumar@hotmail.com>
2018-05-09 16:50:11 -0500

    test cases for callSummary

--- src/test/java/RegressionTesting/CallSummary.java
+++ src/test/java/RegressionTesting/CallSummary.java
@@ -12,13 +12,10 @@ import static org.hamcrest.Matchers.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class CallSummary {
-    String path = "https://partner-api.dev.pltzone.org/v1/reports/usage/tenant/534ebed7-1a74-42c7-8315-608a81aeea8c/callsummary?callStartFromDateTime=2015-06-01T21:06:18.804Z&callEndToDateTime=2019-06-08T21:06:18.804Z&page=0&count=10";
-
-
+    private String path = "https://partner-api.dev.pltzone.org/v1/reports/usage/tenant/534ebed7-1a74-42c7-8315-608a81aeea8c/callsummary?callStartFromDateTime=2015-06-01T21:06:18.804Z&callEndToDateTime=2019-06-08T21:06:18.804Z&page=0&count=10";
 
     @Test
     public void T1_CheckTenantId(){
-
 
         Response resp = given().
                 header("appID", "abcd").
@@ -28,7 +25,7 @@ public class CallSummary {
         //resp.print();
 
         String tenantID = resp.jsonPath().get("body['content'][0]['tenantId']");
-        //System.out.println(tenantID);
+        System.out.println(tenantID);
         Assert.assertEquals(tenantID,"534ebed7-1a74-42c7-8315-608a81aeea8c");
 
 
@@ -37,15 +34,35 @@ public class CallSummary {
     @Test
     public void T2_CheckTenantName(){
 
-        //String path = "https://partner-api.dev.pltzone.org/v1/reports/usage/tenant/534ebed7-1a74-42c7-8315-608a81aeea8c/callsummary?callStartFromDateTime=2016-01-23T00:00:15.872Z&callEndToDateTime=2019-07-23T00:48:44.872Z&page=0&count=10";
-        Response resp = given().
+         Response resp = given().
                 header("appID", "abcd").
                 when().
                 get(path);
 
+
         String tenantName = resp.jsonPath().get("body['content'][0]['tenantName']");
         System.out.println(tenantName);
         Assert.assertEquals(tenantName,"automation");
+
+
+    }
+
+    @Test
+    public void T3_CheckFieldValues(){
+
+        Response resp = given().
+                header("appID", "abcd").
+                when().
+                get(path);
+        resp.print();
+        String family = resp.jsonPath().get("body['content'][0]['product']['family']");
+        Assert.assertNotNull(family);
+        System.out.println(family);
+
+
+//        String tenantName = resp.jsonPath().get("body['content'][0]['tenantName']");
+//        System.out.println(tenantName);
+//        Assert.assertEquals(tenantName,"automation");
 
 
     }
